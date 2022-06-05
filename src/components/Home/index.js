76bbd8422dd3429c5df92ba187b66f76ed16e671
Avinash Chavan <avinashchavan1443@gmail.com>
2022-06-05 20:41:41 +0530

    Home page complete

---
+++ src/components/Home/index.js
@@ -0,0 +1,56 @@
+import './index.scss'
+import logoS from '../../assets/images/logo-s.png'
+import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+import Logo from './Logo'
+
+const Home = () => {
+  const [letterClass, setLettersClass] = useState('text-animate')
+  const nameArray = [...'Avinash']
+  const jobArray = [...'Software Engineer']
+  const updateClass = () => {
+    setLettersClass('text-animate-hover')
+  }
+  const updateClassTimeout = () => {
+    setTimeout(updateClass, 4000)
+  }
+  useEffect(updateClassTimeout, [])
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          {/* Hi, <br /> I'm */}
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+
+          <img src={logoS} alt="developer" />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
+        </h1>
+        <h2>
+          Full Stack Engineer / Software Development Engineer / Javascript
+          Expert
+        </h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+      <Logo />
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,93 @@
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import logoS from '../../assets/images/logo-s.png'
+import logoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import {
+  faLinkedin,
+  faGithub,
+  faYoutube,
+  faInstagram,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons'
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={logoS} alt="logo" />
+        <img className="sub-logo" src={logoSubtitle} alt="avinash" />
+      </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          to="/about"
+          className="about-link"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          to="/contact"
+          className="contact-link"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/avinash-chavan-1a924b142/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/avinashchavan1"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://twitter.com/Avinash3642"
+          >
+            <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.instagram.com/avinashchavan_/"
+          >
+            <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/avinash-chavan-1a924b142/"
+          >
+            <FontAwesomeIcon icon={faYoutube} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
