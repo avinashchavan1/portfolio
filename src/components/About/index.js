7a3faa8a7f7e0da57e83cc05f3b44e3137e601ec
Avinash Chavan <avinashchavan1443@gmail.com>
2022-06-06 14:08:01 +0530

    about page and loader animation

---
+++ src/components/About/index.js
@@ -0,0 +1,82 @@
+import {
+  faAngular,
+  faAws,
+  faDocker,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faNodeJs,
+  faPython,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+
+const About = () => {
+  const [letterClass, setLettersClass] = useState('text-animate')
+  const updateClass = () => {
+    setLettersClass('text-animate-hover')
+  }
+  const updateClassTimeout = () => {
+    setTimeout(updateClass, 3000)
+  }
+  useEffect(updateClassTimeout, [])
+  return (
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={[...'About me']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I am Web Developer with good knowledge of frontend and back end
+            technologies, enjoy Machine learning and have worked with Business
+            Intelligence tools{' '}
+          </p>
+          <p>
+            Enjoy Machine learning and have worked with Business Intelligence
+            tools
+          </p>
+          <p>
+            I am Web Developer with good knowledge of frontend and back end
+            technologies, enjoy Machine learning and have worked with Business
+            Intelligence tools
+          </p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faNodeJs} color="#75ac64" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faDocker} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faPython} color="#3776ab" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default About
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
 import Logo from './Logo'
+import Loader from 'react-loaders'
 
 const Home = () => {
   const [letterClass, setLettersClass] = useState('text-animate')
@@ -17,39 +18,42 @@ const Home = () => {
   }
   useEffect(updateClassTimeout, [])
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          {/* Hi, <br /> I'm */}
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            {/* Hi, <br /> I'm */}
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
 
-          <img src={logoS} alt="developer" />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={22}
-          />
-        </h1>
-        <h2>
-          Full Stack Engineer / Software Development Engineer / Javascript
-          Expert
-        </h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+            <img src={logoS} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>
+            Full Stack Engineer / Software Development Engineer / Javascript
+            Expert
+          </h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
+      <Loader type="pacman" />
+    </>
   )
 }
 
