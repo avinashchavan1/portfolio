29a713e02a8e199468e45e89b7c852e5a5be1d3c
Avinash Chavan <avinashchavan1443@gmail.com>
2022-06-06 11:58:04 +0530

    gsap trail remove

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,28 +1,27 @@
 import './index.scss'
 import logoS from '../../../assets/images/logo-s.png'
 import { useEffect, useRef } from 'react'
-import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+import { gsap } from 'gsap'
 const Logo = () => {
   const bgRef = useRef()
   const outlineLogoRef = useRef()
   const solidLogoRef = useRef()
 
   useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
-
-    gsap.timeline().to(bgRef.current, {
-      duration: 1,
-      opacity: 1,
-    })
-    //   .from(outlineLogoRef.current, {
-    //     drawSVG: 0,
-    //     duration: 20,
-    //   })
+    gsap
+      .timeline()
+      .to(bgRef.current, {
+        duration: 1,
+        opacity: 1,
+      })
+      .from(outlineLogoRef.current, {
+        drawSVG: 0,
+        duration: 20,
+      })
     gsap.fromTo(
       solidLogoRef.current,
       { opacity: 0 },
-      { opacity: 1, delay: 4, duration: 10 }
+      { opacity: 1, delay: 4, duration: 6 }
     )
   }, [])
   return (
