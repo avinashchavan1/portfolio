c9b092d5d45b4a6a35ee008a89cc9404d2180a2b
avinashchavan1 <avinashchavan1443@gmail.com>
2025-07-26 14:48:23 +0530

    feat: make all pages responsive and mobile-friendly

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -9,6 +9,8 @@ import {
   faHome,
   faSuitcase,
   faUser,
+  faBars,
+  faTimes,
 } from '@fortawesome/free-solid-svg-icons'
 import {
   faLinkedin,
@@ -16,90 +18,116 @@ import {
   faInstagram,
   faTwitter,
 } from '@fortawesome/free-brands-svg-icons'
+import { useState } from 'react'
+
 const Sidebar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen)
+  }
+
+  const closeMobileMenu = () => {
+    setIsMobileMenuOpen(false)
+  }
+
   return (
-    <div className="nav-bar">
-      <Link className="logo" to="/">
-        <img src={logoS} alt="logo" />
-        <img className="sub-logo" src={logoSubtitle} alt="avinash" />
-      </Link>
-      <nav>
-        <NavLink exact="true" activeclassname="active" to="/">
-          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-        </NavLink>
-        <NavLink
-          exact="true"
-          activeclassname="active"
-          to="/about"
-          className="about-link"
-        >
-          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-        </NavLink>
-        <NavLink
-          exact="true"
-          activeclassname="active"
-          to="/portfolio"
-          className="portfolio-link"
-        >
-          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
-        </NavLink>
-        <NavLink
-          exact="true"
-          activeclassname="active"
-          to="/contact"
-          className="contact-link"
-        >
-          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-        </NavLink>
-      </nav>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://www.linkedin.com/in/avinash-chavan-1a924b142/"
+    <>
+      <button className="mobile-menu-toggle" onClick={toggleMobileMenu}>
+        <FontAwesomeIcon icon={isMobileMenuOpen ? faTimes : faBars} />
+      </button>
+
+      <div className={`nav-bar ${isMobileMenuOpen ? 'mobile-open' : ''}`}>
+        <Link className="logo" to="/" onClick={closeMobileMenu}>
+          <img src={logoS} alt="logo" />
+          <img className="sub-logo" src={logoSubtitle} alt="avinash" />
+        </Link>
+        <nav>
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            to="/"
+            onClick={closeMobileMenu}
           >
-            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://github.com/avinashchavan1"
+            <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+          </NavLink>
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            to="/about"
+            className="about-link"
+            onClick={closeMobileMenu}
           >
-            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://leetcode.com/chavanavinash/"
+            <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+          </NavLink>
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            to="/portfolio"
+            className="portfolio-link"
+            onClick={closeMobileMenu}
           >
-            <FontAwesomeIcon icon={faCode} color="#4d4d4e" />
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://twitter.com/Avinash3642"
+            <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+          </NavLink>
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            to="/contact"
+            className="contact-link"
+            onClick={closeMobileMenu}
           >
-            <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://www.instagram.com/avinashchavan_/"
-          >
-            <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
-          </a>
-        </li>
-      </ul>
-    </div>
+            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+          </NavLink>
+        </nav>
+        <ul>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/avinash-chavan-1a924b142/"
+            >
+              <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/avinashchavan1"
+            >
+              <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://leetcode.com/chavanavinash/"
+            >
+              <FontAwesomeIcon icon={faCode} color="#4d4d4e" />
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://twitter.com/Avinash3642"
+            >
+              <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.instagram.com/avinashchavan_/"
+            >
+              <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
   )
 }
 
